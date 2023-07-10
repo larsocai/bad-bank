57cb08e919778421b269adf50931e06e61e4b12d
Caitlyn Larson <124397489+larsocai@users.noreply.github.com>
2023-07-10 12:21:21 -0700

    Update NavBar

--- src/containers/navbar.js
+++ src/containers/navbar.js
@@ -6,29 +6,29 @@ function NavBar() {
 		<>
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
             <div className="container-fluid">
-                <Link className="navbar-brand" to="/">BadBank</Link>
+                <a className="navbar-brand" href="/">BadBank</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <Link className="nav-link" to="/createaccount/">Create Account</Link>
+                            <a className="nav-link" href="/createaccount/">Create Account</a>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/login/">Login</Link>
+                            <a className="nav-link" href="/login/">Login</a>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/deposit/">Deposit</Link>
+                            <a className="nav-link" href="/deposit/">Deposit</a>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/withdraw/">Withdraw</Link>
+                            <a className="nav-link" href="/withdraw/">Withdraw</a>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/balance/">Balance</Link>
+                            <a className="nav-link" href="/balance/">Balance</a>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/alldata/">All Data</Link>
+                            <a className="nav-link" href="/alldata/">All Data</a>
                         </li>                        
                     </ul>
                 </div>
@@ -38,4 +38,4 @@ function NavBar() {
 	);
 }
 
-export default NavBar;
+export default NavBar
